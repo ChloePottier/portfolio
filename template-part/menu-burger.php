638cdb885f7e070222a14a4c13d3499567b00f86
ChloePottier <chloe.pottier@free.fr>
2020-10-09 17:12:10 +0200

    deplacement des scripts
    deplacement des scripts
    
    separation des scripts et des menus

---
+++ template-part/menu-burger.php
@@ -0,0 +1,18 @@
+<div class="col">
+    <label class="burger text-center d-flex flex-column d-md-none text-white font-size-13 position-fixed rounded-circle bg-burger" id="burger">MENU
+        <span class="bg-white isclosed mx-auto" id="burger1">&nbsp;</span>
+        <span class="bg-white isclosed mx-auto" id="burger2">&nbsp;</span>
+        <span class="bg-white isclosed mx-auto" id="burger3">&nbsp;</span>
+    </label>
+    <nav class="menu-burger display-none bg-prune-dark-op70 px-auto" id="navigation">
+        <ul class="list-unstyled font-family-bebas font-size-31 text-right pt-5">
+            <li class=""><a href="index.php" class="pb-2 text-decoration-none">Home</a></li>
+            <li class=""><a href="index.php#portfolio" class="pb-2 text-decoration-none" id="portfolio1">Portfolio</a></li>
+            <li class=""><a href="#competences" class="pb-2 text-decoration-none">Compétences</a>
+            </li>
+            <li class=""><a href="index.php#formation" class="pb-2 text-decoration-none">Formation</a></li>
+            <li class=""><a href="index.php#experience" class="pb-2 text-decoration-none">Expérience</a></li>
+            <li class=""><a href="index.php#contact" class="pb-2 text-decoration-none">Contact</a></li>
+        </ul>
+    </nav>
+</div>
