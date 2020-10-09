bcc2023e639e58e74dd72d071c42bbee13a2c7be
ChloePottier <chloe.pottier@free.fr>
2020-10-09 17:22:42 +0200

    separation des scripts et des menus

--- footer.php
+++ footer.php
@@ -52,6 +52,7 @@
             </div>
         </div>
     </footer>
+    <script type="text/javascript" src="js/menu-burger.js"></script>
     <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="js/validator.min.js"></script>
     <script type="text/javascript" src="js/form-scripts.js"></script>
---
+++ template-part/menu-nav.php
@@ -0,0 +1,20 @@
+<div class="container-fluid  nav-top d-none d-md-block nav-top bg-white">
+    <div class="container">
+        <div class="row w-100 bg-white">
+            <div class="col d-flex align-items-center">
+                <a class="logo-CP" href="index.php"></a>
+            </div>
+            <div class="col">
+                <nav class="menu-nav d-none d-md-flex">
+                    <ul class="list-unstyled d-flex justify-content-end font-family-bebas font-size-21 align-items-center mb-0 my-3">
+                        <li class=""><a href="index.php#portfolio" class="text-prune-dark">Portfolio</a></li>
+                        <li class=""><a href="index.php#competences" class="text-prune-dark">Compétences</a></li>
+                        <li class=""><a href="index.php#formation" class="text-prune-dark">Formation</a></li>
+                        <li class=""><a href="index.php#experience" class="text-prune-dark">Expérience</a></li>
+                        <li class=""><a href="index.php#contact" class="text-prune-dark">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
