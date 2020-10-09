00542afd889ba2445acefc57583de0b0bfd06982
ChloePottier <chloe.pottier@free.fr>
2020-10-09 16:57:59 +0200

    nouveau footer captcha

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                     <div class="card__side card__side--front rumbleinn h-250 bg-cover"></div>
                 </a>
             <!-- Garage Ker-Auto-->
-            <a href="https://garagekerauto.fr/" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
+            <!-- <a href="https://garagekerauto.fr/" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
                     <div class="card__side card__side--back card__side--back-1 bg-prune-dark">
                         <div class="card__cta">
                             <div class="page">
@@ -77,7 +77,7 @@
                         </div>
                     </div>
                     <div class="card__side card__side--front kerauto h-250 bg-cover"></div>
-                </a>
+                </a> -->
             <!-- chancel -->
             <a href="http://www.refuge-chancel.com/" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
                     
@@ -151,7 +151,7 @@
                     <div class="card__side card__side--front blueasy h-250 bg-cover"></div>
                 </a>
                 <!-- Vin français -->
-                <a href="../vins-francais" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
+                <!-- <a href="../vins-francais" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
                     <div class="card__side card__side--back card__side--back-1 bg-prune-dark">
                         <div class="card__cta">
                             <div class="page">
@@ -164,7 +164,7 @@
                         </div>
                     </div>
                     <div class="card__side card__side--front vin-fr h-250 bg-cover"></div>
-                </a>
+                </a> -->
                 
                 <!-- flappy mario -->
                 <a href="flappy-mario" target="_blank" class="col-12 col-sm-6 col-md-4 card h-250 mb-3 mb-md-0">
@@ -290,67 +290,13 @@
         </div>
     </div>
     <!-- CONTACT -->
-    <footer>
-        <div class="container-fluid bg-contact" id="contact">
-            <div class="container">
-                <div class="row py-5">
-                    <div class="col-12 col-md-6">
-                        <h1 class="text-prune-dark pt-0 pt-sm-5">Contact</h1>
-                        <form method="POST" class="pb-sm-6 shake" id="contactForm" data-toggle="validator" role="form">
-
-                            <!-- <form action="traitement.php" method="POST" class="pb-sm-6" id="contactForm"> -->
-                            <div class="d-flex flex-column w-75 font-size-18">
-                                <input type="text" name="nom" id="nom" class="bg-white-op40 border-0 text-prune-dark  font-family-bebas pl-3 mb-3" placeholder="Nom *" required />
-                                <div class="help-block with-errors"></div>
-                                <input type="text" name="prenom" id="prenom" class="bg-white-op40 border-0 text-prune-dark  font-family-bebas  pl-3 mb-3" placeholder="Prénom *" required />
-                                <div class="help-block with-errors"></div>
-                                <input type="text" name="societe" id="societe" class="bg-white-op40 border-0 text-prune-dark font-family-bebas  pl-3 mb-3" placeholder="Société" />
-                                <input type="text" name="telephone" id="telephone" class="bg-white-op40 border-0 text-prune-dark  font-family-bebas  pl-3 mb-3" placeholder="Téléphone" />
-                                <input type="email" name="email" id="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,8}$" class="bg-white-op40 border-0 text-prune-dark  font-family-bebas pl-3 mb-3" placeholder="E-mail *" required />
-                                <div class="help-block with-errors"></div>
-                                <textarea name="message" id="message" class="bg-white-op40 border-0 text-prune-dark  font-family-bebas pl-3 mb-3" placeholder="Votre message *" cols="30" rows="10" required></textarea>
-                                <div class="help-block with-errors"></div>
-                            </div>
-                            <!-- <div class="d-flex flex-column pb-5 pb-sm-0"> -->
-                            <div>
-                            <?php include "recaptcha.php"?>
-                            </div>
-                            <button id="form-submit" class="bg-prune-dark font-family-bebas font-size-24 text-blue  border-0" type="submit">ENVOYER</button>
-                            <div id="msgSubmit" class="h3 hidden"></div>
-                            <!-- </div> -->
-                        </form>
-
-                    </div>
-                    <div class="col-12 col-md-6 d-flex justify-content-center flex-column pt-2 pt-md-0">
-                        <!-- retrouvez moi -->
-                        <img src="img/ChloePottier-logo.svg" alt="Chloé Pottier logo" width="289" height="289" class="mx-auto" />
-                        <p class="font-family-bebas text-prune-dark font-size-31 line-height text-center pt-5">
-                            Retrouvez-moi<br />aussi sur :</p>
-                        <div class="d-flex flex-row justify-content-center">
-                            <a href="https://github.com/ChloePottier" target="_blank" class="w-66px github d-flex mr-4"></a>
-                            <a href="https://www.linkedin.com/in/chlo%C3%A9-pottier-b17b60196/" target="_blank" class="ml-4 w-66px linkedin d-flex"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid bg-prune-dark sub-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 text-center text-blue font-size-13 ">
-                        <p class="mb-0 my-3">Mentions légales - Chloé Pottier Portfolio 2020©</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include "footer.php"; ?>
     <!-- Script JQUERY pour Ajax -->
     <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="js/validator.min.js"></script>
     <script type="text/javascript" src="js/form-scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://www.google.com/recaptcha/api.js?render=_reCAPTCHA_site_key"></script>
     <script>
 
     </script>
